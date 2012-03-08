397d97a44552eddac5552c2cedc7d211c70826f4
sjm <sean@ubuntu.(none)>
2012-03-08 14:56:40 -0700

    limiting # of tags that are displayed

--- _plugins/popular_tags.rb
+++ _plugins/popular_tags.rb
@@ -7,7 +7,7 @@ module Jekyll
       context.registers[:site].tags.each { |t| frequencies[t[0]] += t[1].length }
       value = Array.new
       value << "<ol class=\"popular-tags\">"
-      frequencies.sort_by {|k, v| v}.reverse.each do |k, v|
+      frequencies.sort_by {|k, v| v}.reverse[0..9].each do |k, v|
         value << "<li><a href=\"/topic/#{k.downcase.sub(" ","-")}\">#{k.split.each { |x| x.capitalize! }.join(' ')}</a></li>"
       end
       value << "</ol>"
